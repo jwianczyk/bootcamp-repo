1f4b37ee575bba3ca5321e54369dba5d91583110
Jakub Wiańczyk <jwianczyk@gmail.com>
2024-10-27 21:16:23 +0100

    [Feat] Added project 1 & 2 in Jenkins module

---
+++ 08.Jenkins-module/Project2/script.groovy
@@ -0,0 +1,23 @@
+def init(){
+    echo 'Initializing repository'
+    sh 'cd 08.Jenkins-module/Project2'
+}
+
+def buildJar() {
+    echo 'building the application'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t lacroix0/bootcamp_repo:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push lacroix0/bootcamp_repo:jma-2.0'
+    }
+}
+
+def deployApp() {
+    echo 'deploying the application'
+}
+
+return this
